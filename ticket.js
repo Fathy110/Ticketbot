68e05c9ee6eec310b4316eaaa0781f4f90002984
ifireking <67865259+Fathy110@users.noreply.github.com>
2020-09-20 17:51:09 +0200

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -1,39 +1,19 @@
-const { Client, RichEmbed } = require("discord.js");
-var { Util } = require('discord.js');
-const {YT_API_KEY, prefix, devs} = require('./config')
-const client = new Client({ disableEveryone: true})
-const ytdl = require("ytdl-core");
-const canvas = require("canvas");
-const Canvas = require("canvas");
-const convert = require("hh-mm-ss")
-const fetchVideoInfo = require("youtube-info");
-const botversion = require('./package.json').version;
-const simpleytapi = require('simple-youtube-api')
-const moment = require("moment");
-const fs = require('fs');
-const util = require("util")
-const gif = require("gif-search");
-const opus = require("node-opus");
-const ms = require("ms");
-const jimp = require("jimp");
-const { get } = require('snekfetch');
-const guild = require('guild');
-const dateFormat = require('dateformat');//npm i dateformat
-const YouTube = require('simple-youtube-api');
-const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
-const hastebins = require('hastebin-gen');
-const getYoutubeID = require('get-youtube-id');
-const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
-const pretty = require("pretty-ms");
-client.login(process.env.MG_TOKEN);
-const queue = new Map();
-var table = require('table').table
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '#'
 client.on('ready', () => {
-  console.log(`Iam Ready My Owner ${client.user.tag}!`);
+console.log('[ BOT IS ONLINE ]')
+console.log('Alpha support')
+console.log('by   FireKing 🔥#0444')
+});
+client.login("التوكن");
+FireKing.on("ready", () => {
+console.log(`Logged in as ${FireKing.user.tag}!`);
+FireKing.user.setActivity(`online`);
+FireKing.user.setStatus("online"); // الحالة
 });
 
-
+///by FireKing 🔥#0444
 const category = "category-id";
 let mtickets = true;
 let tchannels = [];
